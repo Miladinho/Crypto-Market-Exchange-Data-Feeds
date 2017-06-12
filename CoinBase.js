4725743f3251efa8976e41b333021a6264f06f93
Milad <Milad@Milads-MacBook-Pro.local>
2017-06-11 18:26:33 -0700

    added coinbase module and tests

--- CoinBase.js
+++ CoinBase.js
@@ -1,6 +1,32 @@
-
+var Gdax = require('gdax');
 var exports = module.exports = {}
 
-exports.getTrades = function() {
-
+function currencyStringTransformer(currency) {
+	try {
+		var currencies = currency.split('-');
+		var firstCurrency = currencies[0];
+		var secondCurrency = currencies[1];
+		if (firstCurrency === 'USDT') {
+			return secondCurrency+'-USD';
+		} else {
+			return secondCurrency+'-'+firstCurrency;
+		}
+	} catch(err) {
+		return currency;
+	}
 }
+
+exports.getTrades = function(currency,callback) {
+	var client = new Gdax.PublicClient(currencyStringTransformer(currency));
+	client.getProductTrades({'before': 100}, function(err, response, data) {
+		//console.log(data.message);
+		if (data.message === 'NotFound') callback(true,null);
+		else callback(false,JSON.parse(response.body));
+	});
+}
+
+// var client = new Gdax.PublicClient('BTC-USD');
+// client.getProductTrades({'before': 200}, function(err, result) {
+// 	console.log(JSON.parse(result.body));
+// });
+
--- tests/CoinBase_tests.js
+++ tests/CoinBase_tests.js
@@ -5,6 +5,18 @@ describe('CoinBase module', function() {
 	describe('getTrades', function() {
 		it('should export a function', function() {
 			Chai.expect(CoinBase.getTrades).is.a('function');
-		})
-	})
+		});
+		it('should pass error to callback on bad currency input', function(done) {
+			CoinBase.getTrades('p', function(err, result) {
+				Chai.assert(err === true);
+				done();
+			});
+		});
+		it('should pass a list of objects on successful api call to callback', function(done) {
+			CoinBase.getTrades('USDT-BTC', function(err, result) {
+				Chai.expect(result).to.be.a('array');
+				done();
+			});
+		});
+	});
 })
