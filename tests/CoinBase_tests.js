584b4583fe00459d3eb9d27d66f7647afb1ed66b
Milad <Milad@Milads-MacBook-Pro.local>
2017-06-15 16:31:55 -0700

    refactored and added extra assert in second test case

--- tests/CoinBase_tests.js
+++ tests/CoinBase_tests.js
@@ -1,14 +1,21 @@
 var Chai = require('chai');
-var CoinBase = require('../CoinBase');
+var CoinBase = require('../lib/CoinBase');
 
 describe('CoinBase module', function() {
 	describe('getTrades', function() {
 		it('should export a function', function() {
 			Chai.expect(CoinBase.getTrades).is.a('function');
 		});
-		it('should pass error to callback on bad currency input', function(done) {
+		it('should pass error object to callback on bad currency input', function(done) {
 			CoinBase.getTrades('bad', function(err, result) {
-				Chai.assert(err != null);
+				Chai.assert(err != null,'err parameter should not be null');
+				Chai.assert(err instanceof Error,'err parameter should be an Error object');
+				done();
+			});
+		});
+		it('should pass null for result object in callback on bad currency input', function(done) {
+			CoinBase.getTrades('bad', function(err, result) {
+				Chai.assert(result === null);
 				done();
 			});
 		});
@@ -22,11 +29,12 @@ describe('CoinBase module', function() {
 			CoinBase.getTrades('USDT-BTC', function(err, result) {
 				result.forEach(function(item) {
 					var keys = Object.keys(item);
-					Chai.assert(keys[0] === 'TimeStamp');
-					Chai.assert(keys[1] === 'TradeId');
-					Chai.assert(keys[2] === 'OrderType');
-					Chai.assert(keys[3] === 'Price');
-					Chai.assert(keys[4] === 'Quantity')
+					Chai.assert(keys[0] === 'Exchange');
+					Chai.assert(keys[1] === 'TimeStamp');
+					Chai.assert(keys[2] === 'TradeId');
+					Chai.assert(keys[3] === 'OrderType');
+					Chai.assert(keys[4] === 'Price');
+					Chai.assert(keys[5] === 'Quantity')
 				});
 				done();
 			});
