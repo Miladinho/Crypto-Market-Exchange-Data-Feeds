b7186f801430071ebd9a234c9ced3d8d348de426
Milad <Milad@Milads-MacBook-Pro.local>
2017-07-30 17:33:30 -0700

    refactoring

--- tests/Helpers.js
+++ tests/Helpers.js
@@ -2,10 +2,9 @@ var Chai = require('chai');
 var exports = module.exports = {};
 
 exports.generalGetTradesFunctionTests = function(module) {
-	it('should export a function', function(done) {
-		Chai.expect(module.getTrades).to.be.a('function');
-		done();
-	});
+	/*
+		Note: USDT-BTC is the currency string used in these tests because nearlly all exchanges will support this.
+	*/
 	describe('Callback pattern tests', function() {
 		it('should pass error object to callback on bad currency input and null as reuslt block', function(done) {
 			module.getTrades('',function(err, result) {
@@ -13,7 +12,6 @@ exports.generalGetTradesFunctionTests = function(module) {
 				Chai.assert(result === null);
 				done();
 			});
-
 		});
 		it('should pass list of objects on succesfull api call to callback and null as error', function(done) {
 			module.getTrades('USDT-BTC',function(err, result) {
@@ -23,8 +21,16 @@ exports.generalGetTradesFunctionTests = function(module) {
 			});
 		});
 	});
+	it('should export a function', function(done) {
+		Chai.expect(module.getTrades).to.be.a('function');
+		done();
+	});
 	it('should return objects of uniform keys', function(done) {
 		module.getTrades('USDT-BTC', function(err, result) {
+			if (err) {
+				done(err);
+				return;
+			}
 			result.forEach(function(item) {
 				var keys = Object.keys(item);
 				Chai.assert(keys[0] === 'Exchange');
@@ -32,7 +38,8 @@ exports.generalGetTradesFunctionTests = function(module) {
 				Chai.assert(keys[2] === 'TradeId');
 				Chai.assert(keys[3] === 'OrderType');
 				Chai.assert(keys[4] === 'Price');
-				Chai.assert(keys[5] === 'Quantity')
+				Chai.assert(keys[5] === 'Quantity');
+				Chai.assert(keys[6] === 'OriginalDataObject');
 			});
 			done();
 		});
